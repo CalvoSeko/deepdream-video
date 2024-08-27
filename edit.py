a07786d32a85142b06748bb1fda2a6067e3bb375
Tianwei <90248677+CalvoSeko@users.noreply.github.com>
2024-08-27 11:11:02 +0200

    Example products

--- edit.py
+++ edit.py
@@ -1,4 +1,5 @@
 from moviepy.editor import *
 video = VideoFileClip("output.avi")
 audio = AudioFileClip("original.mp4")
-video.write_videofile("product.mp4")
+audioAndVideo = video.set_audio(audio)
+audioAndVideo.write_videofile("product.mp4")
